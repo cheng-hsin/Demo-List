0578a5f258758b07b0ff58360a97f1346cb22081
cheng-hsin <s950091qq@gmail.com>
2020-10-12 18:36:07 +0800

    Updates

---
+++ precache-manifest.67d1e33611e55c618c144036b28b8a9d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3b4055adea8c0c0052db92f2d2f26f",
+    "url": "/Demo-List/index.html"
+  },
+  {
+    "revision": "46849652c9b7ebf70b35",
+    "url": "/Demo-List/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3b1e33550775567da5de",
+    "url": "/Demo-List/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "46849652c9b7ebf70b35",
+    "url": "/Demo-List/static/js/2.477067de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demo-List/static/js/2.477067de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1e33550775567da5de",
+    "url": "/Demo-List/static/js/main.4ae9bbd1.chunk.js"
+  },
+  {
+    "revision": "1cd8c75b1dbd4b382f71",
+    "url": "/Demo-List/static/js/runtime-main.3c35265b.js"
+  },
+  {
+    "revision": "30ca8c7a8390fa4a956b70a5f723a738",
+    "url": "/Demo-List/static/media/calculator.30ca8c7a.PNG"
+  },
+  {
+    "revision": "5ad977b3574ab58da7fc99f9bfd15bd3",
+    "url": "/Demo-List/static/media/card.5ad977b3.PNG"
+  },
+  {
+    "revision": "473e6cb5b3353125920a6424798255ab",
+    "url": "/Demo-List/static/media/class.473e6cb5.PNG"
+  },
+  {
+    "revision": "1644d3b64c5cf9dfd5bf163b450f143e",
+    "url": "/Demo-List/static/media/food.1644d3b6.PNG"
+  },
+  {
+    "revision": "adb22b1ee55ccbb0d6ddf9199f12245c",
+    "url": "/Demo-List/static/media/monster.adb22b1e.PNG"
+  },
+  {
+    "revision": "da98e4b5e69804161376f51bbcd467fd",
+    "url": "/Demo-List/static/media/movies.da98e4b5.PNG"
+  },
+  {
+    "revision": "50ae4c7d532fdd176fc2da6b87b6c80d",
+    "url": "/Demo-List/static/media/music.50ae4c7d.PNG"
+  },
+  {
+    "revision": "784b4bccf55b3b84a5f2d02cc8696fa3",
+    "url": "/Demo-List/static/media/project.784b4bcc.PNG"
+  },
+  {
+    "revision": "10f367fca3de8d851932b02872ec865f",
+    "url": "/Demo-List/static/media/url.10f367fc.PNG"
+  },
+  {
+    "revision": "d979dcb26b1df97dd127e5466e7f76ab",
+    "url": "/Demo-List/static/media/video.d979dcb2.PNG"
+  }
+]);
